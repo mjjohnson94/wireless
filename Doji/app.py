d3fed0caa4ad695bc72aa10454a72573e7800932
mjjohnson94 <82873273+mjjohnson94@users.noreply.github.com>
2022-12-10 14:17:56 +1100

    Add files via upload

--- Doji/app.py
+++ Doji/app.py
@@ -9,3 +9,20 @@ app = Flask(__name__)
 def landingPage():
         return render_template('landingPage.html')
 
+
+@app.route('/', methods=['POST'])
+def registerUser():
+
+        firstName = request.form['firstName']
+        secondName = request.form['secondName']
+        eMail = request.form['eMail']
+        phoneNumber = request.form['phoneNumber']
+        password = request.form['password']
+        confirmPassword = request.form['confirmPassword']
+
+        registrationForm = [firstName, secondName, 
+                           eMail, phoneNumber, 
+                           password, confirmPassword]
+
+        return render_template('index.html', text=firstName)
+
