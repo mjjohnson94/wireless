c9a6c33a7bac23c877fc8eaa29c4b116cfbf9945
mjjohnson94 <82873273+mjjohnson94@users.noreply.github.com>
2022-12-10 14:30:33 +1100

    Add files via upload

--- Doji/app.py
+++ Doji/app.py
@@ -14,15 +14,9 @@ def landingPage():
 def registerUser():
 
         firstName = request.form['firstName']
-        secondName = request.form['secondName']
-        eMail = request.form['eMail']
-        phoneNumber = request.form['phoneNumber']
-        password = request.form['password']
-        confirmPassword = request.form['confirmPassword']
+        secondName = request.form['lastName']
+        
+     
 
-        registrationForm = [firstName, secondName, 
-                           eMail, phoneNumber, 
-                           password, confirmPassword]
-
-        return render_template('index.html', text=firstName)
+        return render_template('index.html', text=firstName, x=secondName)
 
