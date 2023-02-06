524c7758ad6e95c5ba8dbccb7bb84556c0833b52
mjjohnson94 <82873273+mjjohnson94@users.noreply.github.com>
2023-02-06 19:10:55 +1100

    Add files via upload

--- Doji/app.py
+++ Doji/app.py
@@ -1,3 +1,4 @@
+from telnetlib import theNULL
 from flask import Flask, render_template, request
 import pandas as pd
 import psycopg2
@@ -10,18 +11,42 @@ def landingPage():
         return render_template('landingPage.html')
 
 
-@app.route('/', methods=['POST'])
-def registerUser():
+# @app.route('/', methods=['POST'])
+# def registerUser():
 
-        firstName = request.form['firstName']
-        secondName = request.form['lastName']
-        eMail = request.form['eMail']
-        phoneNumber = request.form['phoneNumber']
-        password = request.form['password']
+#     firstName = request.form['firstName']
+#     secondName = request.form['lastName']
+#     eMail = request.form['eMail']
+#     phoneNumber = request.form['phoneNumber']
+#     password = request.form['password']
 
-        ID = 1
+#     ID = 3
 
-        # Establish connection to PostgreSQL database 
+#     # Establish connection to PostgreSQL database
+
+#     conn = psycopg2.connect(
+#         host="localhost",
+#         database="pythontestdb",
+#         user="postgres",
+#         password="AAA009wn73ed")
+
+#     cur = conn.cursor()
+
+#     cur.execute("INSERT INTO registered_users(ID, FIRST_NAME, LAST_NAME, EMAIL_ADDRESS, PHONE_NUMBER, PASSWORD) VALUES(%s, %s, %s, %s, %s, %s)",
+#                 (ID, firstName, secondName, eMail, phoneNumber, password))
+
+#     conn.commit()
+#     cur.close()
+#     conn.close()
+
+#     return render_template('index.html')
+
+
+@app.route('/logIn', methods=['POST'])
+def logIn():
+
+        eMail = request.form['Email']
+        password = request.form['PassW']
 
         conn = psycopg2.connect(
         host="localhost",
@@ -31,16 +56,18 @@ def registerUser():
 
         cur = conn.cursor()
 
-        cur.execute("INSERT INTO registered_users(ID, FIRST_NAME, LAST_NAME, EMAIL_ADDRESS, PHONE_NUMBER, PASSWORD) VALUES(%s, %s, %s, %s, %s, %s)",
-        (ID, firstName, secondName, eMail, phoneNumber, password))
+        cur.execute('SELECT * FROM registered_users WHERE email_address LIKE %s AND password LIKE %s', (eMail, password,))
+        x = cur.fetchall()
 
-        conn.commit()
-        cur.close()
-        conn.close()
-
-        return render_template('index.html')
-
+        if len(x) > 0:
+                return render_template('loggedIn.html')
+        else:
+                return render_template('logInFail.html')
 
 
+@app.route('/registerUser', methods=['POST'])
+def registerUser():
+
+        return render_template('registerUser.html')
 
 
