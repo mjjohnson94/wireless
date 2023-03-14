8a346ff29e703399e530ae5c3a9a87077b3a2b93
mjjohnson94 <82873273+mjjohnson94@users.noreply.github.com>
2023-03-14 20:01:16 +1100

    Add files via upload

--- Doji/static/js/app.js
+++ Doji/static/js/app.js
@@ -1,47 +1,27 @@
-// // Create Chart.js configuration object, this acts as the top level structure for a Chart.js visualisation 
+// Create Chart.js configuration object, this acts as the top level structure for a Chart.js visualisation 
 
-// const config = {
-//     // Select chart type as line
-//     type: 'line',
-//     // Pass data object to the data argument, the object will be defined in the next step 
-//     data: data,
-// };
+const config = {
+    // Select chart type as line
+    type: 'line',
+    // Pass data object to the data argument, the object will be defined in the next step 
+    data: data,
+};
 
-// // Create object named labels which will store each month of the year 
-// // Chart.js provides utilities to assist in building visualisations 
+// Create object named labels which will store each month of the year 
+// Chart.js provides utilities to assist in building visualisations 
 
-// const labels = Utils.months({count: 7});
+const labels = Utils.months({count: 7});
 
-// // Create data object
+// Create data object
 
-// const data = {
-//     labels: labels,
-//     datasets: [{
-//         label: 'My First Dataset',
-//         data: [65, 59, 80, 81, 56, 55, 40],
-//         fill: false,
-//         borderColor: 'rgb(75, 192, 192)',
-//         tension: 0.1
-//       }]
-// };
-
-const ctx = document.getElementById('myChart');
-      
-new Chart(ctx, {
-  type: 'bar',
-  data: {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+const data = {
+    labels: labels,
     datasets: [{
-      label: '# of Votes',
-      data: [12, 19, 3, 5, 2, 3],
-      borderWidth: 1
-    }]
-  },
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true
-      }
-    }
-  }
-});
+        label: 'My First Dataset',
+        data: [65, 59, 80, 81, 56, 55, 40],
+        fill: false,
+        borderColor: 'rgb(75, 192, 192)',
+        tension: 0.1
+      }]
+};
+
